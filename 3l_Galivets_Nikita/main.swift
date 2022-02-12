64e8e6877b370a13ba5a8c3dffef5b266aaab3d7
Hrezolit <Hrezolitaz@gmail.com>
2022-02-12 17:02:26 +0600

    add changes in Homework 3

--- 3l_Galivets_Nikita/main.swift
+++ 3l_Galivets_Nikita/main.swift
@@ -14,85 +14,28 @@ import Foundation
 //5. Инициализировать несколько экземпляров ваших структур. Применить к ним различные действия.
 //6. Вывести значения свойств экземпляров в консоль.
 
-//MARK: - Actions in enum
+//Вариант 1
+//MARK: - Truck Structure
 
 enum Actions {
-    
     case openWindows
     case closeWindows
     case startEngine
     case stopEngine
     case putInTrunk
     case takeFromTrunk
-    
 }
 
-//MARK: - Light car Structure
-
-struct LightCar {
-    
-    private var isEngingRunning = false
-    private var isWindowsOpened = false
-    private let isTrunkFull = false
-    private let brand: String
-    private let model: String
-    private let dateOfRealease: String
-    private let trunkVolume: UInt
-    private var trunkFullness: UInt
-    
-    init(brand: String , model: String, dateOfRealease: String, trunkVolume: UInt, trunkFullness: UInt) {
-        self.brand = brand
-        self.model = model
-        self.dateOfRealease = dateOfRealease
-        self.trunkVolume = trunkVolume
-        self.trunkFullness = trunkFullness
-    }
-    
-    //Метод для взаимодействия с авто
-    mutating func action( _ of: Actions) {
-        
-        switch of {
-            
-        case .openWindows:
-            isWindowsOpened = true
-            print("Windows were opened")
-        case .closeWindows:
-            isWindowsOpened = false
-            print("Windows were closeded")
-            
-        case .startEngine:
-            isEngingRunning = true
-            print("Car engine is power on")
-        case .stopEngine:
-            isEngingRunning = false
-            print("Car engine is power off")
-            
-        case .putInTrunk:
-            trunkFullness != trunkVolume ? trunkFullness += 5 : print("Trunk is full!")
-        case .takeFromTrunk:
-            trunkFullness != 0 ? trunkFullness -= 5 : print("Trunk is empty!")
-        }
-    }
-    //Метод для выведения информации о состоянии авто в консоль
-    func status() {
-        
-        print("\(brand) - \(model), release date: \(dateOfRealease), trunk volume = \(trunkVolume) liters, trunk fullness - \(trunkFullness)")
-    }
-}
-
-
-
-//MARK: - Truck Structure
 struct Truck {
     
-    private var isEngingRunning = false
-    private var isWindowsOpened = false
-    private let isTrunkFull = false
-    private let brand: String
-    private let model: String
-    private let dateOfRealease: String
-    private let trunkVolume: UInt
-    private var trunkFullness: UInt
+    var isEngingRunning = false
+    var isWindowsOpened = false
+    let isTrunkFull = false
+    let brand: String
+    let model: String
+    let dateOfRealease: String
+    let trunkVolume: UInt
+    var trunkFullness: UInt
     
     init(brand: String , model: String, dateOfRealease: String, trunkVolume: UInt, trunkFullness: UInt) {
         self.brand = brand
@@ -138,24 +81,117 @@ struct Truck {
 
 
 
+//><><<><><><><><----(@)-----><><<><><><><><-----(@)----><><<><><><><><-----(@)----><><<><><><><><-------(@)-----><><<><
+//-----(@)----><><<><><><><><-----(@)----><><<><><><><><-----(@)----><><<><><><><><-----(@)----><><<><><><><><------(@)-
+//><><<><><><><><----(@)-----><><<><><><><><-----(@)----><><<><><><><><-----(@)----><><<><><><><><-------(@)-----><><<><
+//-----(@)----><><<><><><><><-----(@)----><><<><><><><><-----(@)----><><<><><><><><-----(@)----><><<><><><><><------(@)-
 
 
 
+//Вариант 2
+
+//MARK: - Light car Structure
+
+enum Windows {
+    case open
+    case close
+}
+
+enum Engine {
+    case start
+    case stop
+}
+
+enum Trunk {
+    case putInTrunk
+    case takeFromTrunk
+}
+
+struct LightCar {
+    
+    let brand: String
+    let model: String
+    let dateOfRealease: String
+    var engineCondition: Bool
+    var widowsCondition: Bool
+    var trunkFullness: UInt
+    var trunkConditionNow: UInt {
+        didSet {
+            if trunkConditionNow > trunkFullness {
+                trunkConditionNow = oldValue
+                print("Trunk is full!")
+            } else if trunkConditionNow == 0 {
+                trunkConditionNow = oldValue
+                print("Trunk is empty")
+            } else {
+                print("Trunk free space (\(trunkConditionNow) / \(trunkFullness)) liters")
+            }
+        }
+    }
+    
+    init(brand: String , model: String, dateOfRealease: String, trunkConditionNow: UInt, trunkFullness: UInt, widowsCondition: Bool, engineCondition: Bool) {
+        self.brand = brand
+        self.model = model
+        self.dateOfRealease = dateOfRealease
+        self.trunkFullness = trunkFullness
+        self.trunkConditionNow = trunkConditionNow
+        self.widowsCondition = widowsCondition
+        self.engineCondition = engineCondition
+    }
+    
+    //Взаимодействие с багажником положить/взять
+    mutating func trunkAction(type: Trunk) {
+        switch type {
+        case .putInTrunk:
+            trunkConditionNow += 50
+        case .takeFromTrunk:
+            trunkConditionNow -= 50
+        }
+    }
+    
+    //Открытие/Закрытие окон
+    mutating func windowsAction(type: Windows) {
+        switch type {
+        case .open:
+            widowsCondition = true
+            print("Windows were opened")
+        case .close:
+            widowsCondition = false
+            print("Windows were closed")
+        }
+    }
+    
+    //Запуск/Остановка двигателя
+    mutating func engineAction(type: Engine) {
+        switch type {
+        case .start:
+            engineCondition = true
+            print("Car engine is power on")
+        case .stop:
+            engineCondition = false
+            print("Car engine is power off")
+        }
+    }
+    
+    
+    //Метод для выведения информации о состоянии авто в консоль
+    func status() {
+        
+        print("\(brand) - \(model), release date: \(dateOfRealease), trunk fullness - \(trunkFullness)")
+    }
+}
 
 
+//MARK: -  Вывод на консоль
 
-var car = LightCar(brand: "Tesla", model: "Model X", dateOfRealease: "2017", trunkVolume: 300, trunkFullness: 295)
+var car = LightCar(brand: "Tesla", model: "Model X", dateOfRealease: "2017", trunkConditionNow: 200, trunkFullness: 300, widowsCondition: false, engineCondition: false)
 
 car.status()
-print("""
-
-""")
-car.action(.startEngine)
-car.action(.openWindows)
-car.action(.putInTrunk)
-
-
-
+car.engineAction(type: .start)
+car.windowsAction(type: .open)
+car.trunkAction(type: .putInTrunk)
+car.trunkAction(type: .putInTrunk)
+car.trunkAction(type: .putInTrunk)
 
 print("""
 
@@ -163,15 +199,13 @@ print("""
 
 """)
 
-var truck = Truck(brand: "Tesla", model: "Sammy", dateOfRealease: "2023", trunkVolume: 3500, trunkFullness: 900)
+var truck = Truck(brand: "Tesla", model: "Sammi", dateOfRealease: "2023", trunkVolume: 2500, trunkFullness: 3000)
 
 truck.status()
 print("""
 
 """)
-truck.action(.stopEngine)
-truck.action(.closeWindows)
+truck.action(.takeFromTrunk)
+truck.action(.startEngine)
+truck.action(.openWindows)
 truck.action(.putInTrunk)
-
-
-//Test commit - refresh HW3
